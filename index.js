4256d74cd21959e2e0ea789652863821f3209fd2
AniThakur00 <ani1998.at@gmail.com>
2023-12-02 21:36:31 -0500

    testing 0.0.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from auth. TESTING 2. container port - 3001, service port - 80.\n');
+  res.end('Hello from auth. TESTING 3. container port - 3001, service port - 80.\n');
 });
 
 const port = 3001;
