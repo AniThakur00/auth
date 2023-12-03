9337db5edbdf1433338ed5761580785f3a103ffa
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-12-03 17:52:41 -0500

    added real code

---
+++ routes/auth.js
@@ -0,0 +1,152 @@
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require("express-validator")
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth")
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - lastName
+ *         - email
+ *         - purchases
+ *         - userInfo
+ *         - encry_password
+ *         - salt
+ *         - role
+ *       properties:
+ *         name:
+ *           type: string
+ *         lastName:
+ *           type: string
+ *         purchases:
+ *           type: array
+ *         userinfo:
+ *           type: object
+ *         encry_password:
+ *           type: string
+ *         salt:
+ *           type: string
+ *         role:
+ *           type: string
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ *         finished: false
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ *     UserReq:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       example:
+ *         email: me@tv.com
+ *         password: ASecretPassword
+  *     SignUpReq:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       example:
+ *         name: Tanay Van
+ *         email: me@tv.com
+ *         password: ASecretPassword
+ */
+
+
+
+/**
+ * @swagger
+ * /signin:
+ *   post:
+ *     summary: Sign in a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserReq'
+ *     responses:
+ *       200:
+ *         description: Use to sign in user.
+ *         content:
+ *           application/json:
+*             schema:
+ *               type: object
+ *               properties:
+ *                 success: error message
+ *       500:
+ *         description: Some server error
+ *         content:
+ *           application/json:
+*             schema:
+ *               type: object
+ *               properties:
+ *                 error: error message
+ * /signup:
+ *   post:
+ *     summary: Sign up a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/SignUpReq'
+ *     responses:
+ *       200:
+ *         description: Use to sign up user.
+ *         content:
+ *           application/json:
+*             schema:
+ *               type: object
+ *               properties:
+ *                 success: error message
+ *       500:
+ *         description: Some server error
+ *         content:
+ *           application/json:
+*             schema:
+ *               type: object
+ *               properties:
+ *                 error: error message
+ *                   
+ */
+router.post('/signup', [
+    check("name", "name should be atleast 3 character").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be atleast 6 character").isLength({ min: 6 }),
+], signup)
+
+router.post('/signin', [
+
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").isLength({ min: 6 }),
+], signin)
+
+
+
+
+
+router.get('/signout', signout)
+
+
+module.exports = router
---
+++ tests/auth.test.js
@@ -0,0 +1,87 @@
+const request = require('supertest');
+const { app, server } = require('../index'); // Import your Express app
+
+describe('User Authentication API', () => {
+    afterAll(() => {
+        server.close();
+    });
+    let token; // This variable will hold the token for authenticated requests
+    let email = 'test7@example.com'
+    // Test user signup
+    it('should sign up a new user', async () => {
+        const response = await request(app)
+            .post('/api/signup')
+            .send({
+                name: 'Test User',
+                email: email,
+                password: 'testpassword',
+            });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('email');
+        token = response.body.token; // Save the token for future requests
+    });
+
+    // Test user signin
+    it('should sign in an existing user', async () => {
+        const response = await request(app)
+            .post('/api/signin')
+            .send({
+                email: email,
+                password: 'testpassword',
+            });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+        token = response.body.token; // Update the token for future requests
+    });
+
+    // Test protected route
+    // it('should access a protected route with a valid token', async () => {
+    //     const response = await request(app)
+    //         .get('/protected-route')
+    //         .set('Authorization', `Bearer ${token}`);
+
+    //     expect(response.statusCode).toBe(200);
+    //     // Add more assertions as needed
+    // });
+
+    // Test signout
+    it('should sign out a user', async () => {
+        const response = await request(app).get('/api/signout');
+
+        expect(response.statusCode).toBe(200);
+        // Add more assertions as needed
+    });
+
+    // Add more tests for isAuthenticated, isAdmin, and other scenarios
+
+    // Example for testing isAuthenticated middleware
+    // it('should deny access if user is not authenticated', async () => {
+    //     const response = await request(app)
+    //         .get('/protected-route')
+    //         .set('Authorization', 'Bearer invalidtoken');
+
+    //     expect(response.statusCode).toBe(403);
+    //     // Add more assertions as needed
+    // });
+
+    // Example for testing isAdmin middleware
+    // it('should deny access if user is not an admin', async () => {
+    //     // Sign in a non-admin user or use a user without admin privileges
+    //     // ...
+
+    //     const response = await request(app)
+    //         .get('/admin-route')
+    //         .set('Authorization', `Bearer ${token}`);
+
+    //     expect(response.statusCode).toBe(403);
+    //     // Add more assertions as needed
+    // });
+
+
+
+
+});
+
+
